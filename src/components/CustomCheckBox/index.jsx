1ebc63a53339785579c16836dcae4b03bf9e0fb8
UXDESIGNERLIN <hochialing91@gmail.com>
2023-01-19 18:42:03 +0100

    Removed comments

--- src/components/CustomCheckBox/index.jsx
+++ src/components/CustomCheckBox/index.jsx
@@ -1,16 +1,8 @@
-import { useState } from "react"
-
 function CustomCheckBox({ task, toggleCheckbox, taskIndex, item }) {
   const {description} = task;
-  //const [onChangedTask, setOnChangedTask] = useState(task);
 
   const handleChange = () => {
- /*    setOnChangedTask(prevTask => {
-      toggleCheckbox({...prevTask, checked: !prevTask.checked}, index);
-      return {...prevTask, checked: !prevTask.checked};
-    }); */ 
     toggleCheckbox({...task, checked: !task.checked}, taskIndex, item);
-
   };
 
   return(
