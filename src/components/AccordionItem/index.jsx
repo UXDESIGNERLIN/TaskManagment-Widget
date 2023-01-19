a314e906456a84d03e4e2631854721472266a54a
UXDESIGNERLIN <hochialing91@gmail.com>
2023-01-19 19:02:00 +0100

    Added color changing behavior according to the checked tasks

--- src/components/AccordionItem/index.jsx
+++ src/components/AccordionItem/index.jsx
@@ -1,18 +1,21 @@
 import CustomCheckBox from "../CustomCheckBox";
 import { Title } from './style';
 
-function AccordionItem({ children }) {
-/*   const { name, tasks } = item;
-  const onChangeCheckValue = (onChangeTask, index) => {
-    console.log('test',onChangeTask, index)
-  } */
-  // use composition return different header and put into the code
-/*   const allTasksChecked = tasks.every(section => 
+function AccordionItem({ children, item  }) {
+  const { tasks, name } = item;
+  const allTasksChecked = tasks.every(section => 
     section.checked
-  ); */
+  );  
 
-  return <div>{children}</div>
-     /*  {allTasksChecked ? <Title>{name}</Title> : <h1>{name}</h1>} */
+  return ( 
+    <div>
+      {allTasksChecked ? 
+        <Title>{name}</Title> : 
+        <h1>{name}</h1>
+      }
+      {children}
+    </div>
+  ) 
 }
   
 export default AccordionItem;
