1f4f4ea2e0c2a4cdea2f045dd066f43b3a358d1d
UXDESIGNERLIN <hochialing91@gmail.com>
2023-01-19 04:22:30 +0100

    Logic for doing normalizing

--- src/components/ProgressBar/index.jsx
+++ src/components/ProgressBar/index.jsx
@@ -1,9 +1,31 @@
 import { Progress, Bar } from './style'
-const ProgressBar = ({ariaValuenow}) => {
+const ProgressBar = ({ tasks }) => {
+
+  const sumAllValues = (data) => {
+    return data.reduce((total, category) => {
+      return total + category.tasks.reduce((subTotal, task) => {
+        return subTotal + task.value;
+      }, 0)
+    }, 0) 
+  }
+
+  const sumCheckedValues = (data) => {
+    return data.reduce((total, category) => {
+      return total + category.tasks.reduce((subTotal, task) => {
+        return task.checked ? subTotal + task.value : subTotal;
+      }, 0);
+    }, 0);
+  }
+
+  const progress = ((sumCheckedValues * 100) / sumAllValues).toFixed(2);
+
+
+
+
   return(
     <>
     <Progress>
-      <Bar role="progressbar" percentage={ariaValuenow} aria-valuenow={ariaValuenow} aria-valuemin="0" aria-valuemax="100">25</Bar>
+      <Bar role="progressbar" percentage={percentage} aria-valuenow={percentage} aria-valuemin="0" aria-valuemax="100">25</Bar>
     </Progress>
     </>
   )
