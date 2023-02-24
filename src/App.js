a44a8bf461c685e246b331314ee21c131ffdd91e
UXDESIGNERLIN <hochialing91@gmail.com>
2023-02-24 15:46:02 +0100

    Refactor

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import TaskManagement from './widgets/TaskManagement';
 
 function App() {
-  const header = 'Lodgify Grouped Tasks'
+  const header = 'Grouped Tasks'
   return (
     <div className="App">
       <TaskManagement header={header}/>
